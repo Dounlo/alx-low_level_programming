8290456aa598905448676b6c65bd7b97fa0fcdd3
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-16 03:50:57 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Edit point
+ *
+ * This a program to write number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int h;
+
+	h = 0;
+
+	for (h = 0; h < 10; h++)
+	{
+		printf("%d", h);
+	}
+	printf("\n");
+	return (0);
+}
