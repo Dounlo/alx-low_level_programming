6c86c377684d4e3fe204da0c5f2a5336d25dddd1
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-16 04:01:50 -0700

    Printing with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Edit point
+ *
+ * This a program that prints
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char nl;
+
+	a = 0;
+	nl = '\n';
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar(nl);
+	return (0);
+}
+
