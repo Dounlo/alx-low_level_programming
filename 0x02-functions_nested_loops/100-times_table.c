3901d0d08799daae507a3e992cb8c4a32ddf5e0b
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-21 05:48:18 -0700

    n table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 0; x < n; x++)
 		{
-			for (y = 0; y <= n; y++)
+			for (y = 0; y < n; y++)
 			{
 				o = x * y;
 				if (y == 0)
