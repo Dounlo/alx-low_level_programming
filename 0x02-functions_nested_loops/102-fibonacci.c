8c6a6d12cc41bb622f567b90f65e37a209092c1b
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-21 14:29:57 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include  <stdio.h>
+/**
+ * main -Edit point
+ * This program prints fibonacci
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int s1, s2, s;
+
+	s1 = 0;
+	s2 = 1;
+	i = 1;
+	while (i <= 50)
+	{
+		s = s1 + s2;
+		s1 = s2;
+		s2 = s;
+		if (i == 50)
+		{
+			printf("%ld\n", s);
+		}
+		else
+		{
+			printf("%ld, ", s);
+		}
+		i++;
+	}
+	return (0);
+}
