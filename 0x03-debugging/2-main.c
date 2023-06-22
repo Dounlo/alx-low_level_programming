89db3f6c676de7cf8bd1511ae1a84fc4bc553b3e
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-22 02:32:40 -0700

    Largest

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
         int a, b, c;
         int largest;
 
-        a = -78;
+        a = 0;
         b = 98;
-        c = -67;
+        c = 98;
 
         largest = largest_number(a, b, c);
 
