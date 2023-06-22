b24d1ed5b038562d126f44e6d12b3b63fb54975c
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-22 14:09:09 -0700

    Function multiplifer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul -multiplies two numbers
+ * @a: the first number to multiply
+ * @b: the second number to multiply
+ * Return: the number multiplied
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
