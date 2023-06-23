5d73a909a16e350f898d2038a098b54c5d01d1c2
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-23 02:08:00 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers
+ * This program prints numbers from 1 to 100
+ * fizz for multipliers of 3
+ * buzz dor multipliers of 5
+ * and fizzbuzz for multipliers of 3 and 5
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
