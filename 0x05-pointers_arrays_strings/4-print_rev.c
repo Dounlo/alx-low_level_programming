dfc89ddad4aad73c78f8fcaebda7e0ac02a41e95
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-27 02:39:30 -0700

    I can only go one way.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string backround
+ * @s: the string to prints
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i + 1;
+	}
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
