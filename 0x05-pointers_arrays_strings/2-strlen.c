09015c480b931eff63691f1d582a390c61f42d24
Dounlo <elouazdiya.dounia93@gmail.com>
2023-06-27 01:50:33 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: the length of s
+ */
+int _strlen(char *s)
+{
+	int len, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i;
+	}
+	return (len);
+}
