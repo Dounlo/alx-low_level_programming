bdea525a520f7b3455e13213bcb559ec4f946ef0
Dounlo <elouazdiya.dounia93@gmail.com>
2023-07-03 14:59:16 -0700

    copu byte

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- *
+ * _memcpy - copies memory area
+ * @dest: destination of memory data
+ * @src: source of mempry data
+ * @n: bytes from memory area
+ * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
