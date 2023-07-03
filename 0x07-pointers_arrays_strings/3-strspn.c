401f3477bb1f471bdb1368ea6d39ada80f5f8fa6
Dounlo <elouazdiya.dounia93@gmail.com>
2023-07-03 15:41:06 -0700

    RELAX

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,24 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n;
-	int i, j, c;
+	int i;
 
-	c = 0;
 	n = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[i])
 			{
-				c++;
-				n = c;
+				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+			{
+				return (n);
 			}
 		}
+		s++;
 	}
 	return (n);
 }
