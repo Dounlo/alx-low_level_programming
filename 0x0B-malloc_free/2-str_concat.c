c71d2645c20dc5174ccd752edecc96d3fef274cd
Dounlo <elouazdiya.dounia93@gmail.com>
2023-07-11 05:35:37 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,14 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, l, z, m;
 	char *pointer;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[i])
 	{
 		i++;
