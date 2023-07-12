1364cf7308ddfda667cac090b3307ece02406d3b
Dounlo <elouazdiya.dounia93@gmail.com>
2023-07-12 03:11:02 -0700

    ha atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,49 @@
 #include "main.h"
 #include "stdio.h"
-#include "stdlib.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: arguments
  *
- * Return : 0 success or 1 error
+ * Return: 0 success or 1 error
  */
 int main(int argc, char *argv[])
 {
@@ -17,7 +54,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	mu = atoi(argv[1]) * atoi(argv[2]);
+	mu = _atoi(argv[1]) * _atoi(argv[2]);
 	printf("%d\n", mu);
 	return (0);
 }
