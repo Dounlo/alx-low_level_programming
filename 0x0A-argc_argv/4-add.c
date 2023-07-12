cca2db119f7b4ea319702edabe0a6cdeae89dc3b
Dounlo <elouazdiya.dounia93@gmail.com>
2023-07-12 10:25:55 -0700

    Did it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,32 @@
 #include "stdio.h"
 #include "main.h"
 #include "stdlib.h"
+#include <ctype.h>
+#include "string.h"
+/**
+ * check_dig - checks a char is a digit
+ * @s: the sring to check
+ *
+ * Return: 1 if success else 0
+ */
+int check_dig(char *s)
+{
+	int i, n;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+	{
+		if (isdigit(s[i]) == 0)
+		{
+			n = 0;
+			break;
+		}
+		else
+		{
+			n = 1;
+		}
+	}
+	return (n);
+}
 /**
  * main - adds unsigned numbers
  * @argc: number of arguments
@@ -12,13 +38,21 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (arc < 1)
+	if (argc < 1)
 	{
 		printf("0\n");
 	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		sum = sum + atoi(argv[i]);
+		if (check_dig(argv[i]) == 1)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (0);
+		}
 	}
 	printf("%d\n", sum);
 	return (0);
