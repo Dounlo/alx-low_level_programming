7279fa6a95612982151104e1dac0f5809af3e44a
Dounlo <elouazdiya.dounia93@gmail.com>
2023-07-13 09:57:29 -0700

    p

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,8 +13,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *pointer, *old;
 	unsigned int i;
 
-	if (new_size == old_size)
-		return (NULL);
+	if (new_size == old_size && ptr != NULL)
+		return (ptr);
 	if (ptr == NULL)
 		return (malloc(new_size));
 	if (new_size == 0 && ptr != NULL)
