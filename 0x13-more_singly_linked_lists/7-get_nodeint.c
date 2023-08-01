3e454c2ad0a4de6e3a941822da27196eaa726bba
Dounlo <elouazdiya.dounia93@gmail.com>
2023-08-01 02:09:06 -0700

    Good morning

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current = head;
 	unsigned int i;
 
+	i = 0;
 	while (current && i < index)
 	{
 		current = current->next;
