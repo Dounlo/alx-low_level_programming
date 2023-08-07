06d7419eebee60ea5b0caddae3b0efab6a14b568
Dounlo <elouazdiya.dounia93@gmail.com>
2023-08-07 03:12:32 -0700

    hop

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,16 @@
 #include "main.h"
 /**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: A pointer to the name of the file
+ * @letters: The number of letters the function should read and print
  *
+ * Return: the actual number of letters it could read and print
+ * Or 0 if it fails
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t rd, opn, wrt;
-	size_t *poin;
+	ssize_t rd, opn, wrt;
+	char *poin;
 
 	poin = malloc(sizeof(char) * letters);
 	if (poin == NULL || filename == NULL)
@@ -13,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	opn = open(filename, O_RDONLY);
 	rd = read(opn, poin, letters);
 	wrt = write(STDOUT_FILENO, poin, rd);
-	if (opn == -1 || rd == -1 || wrt == -1 || wrt != read)
+	if (opn == -1 || rd == -1 || wrt == -1 || wrt != rd)
 	{
 		free(poin);
 		return (0);
