13fd176bdcb3176c215a86b7bc6edeb8942bb5f1
Dounlo <elouazdiya.dounia93@gmail.com>
2023-08-07 04:56:03 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[length])
 			length++;
 	}
-	opn = open(filename, O_CREAT || O_RDWR || O_TRUNC, 0600);
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(opn, text_content, length);
 
 	if (opn == -1 || wrt == -1)
